637e6c2226c3ae3c2ced47b1cf91eda9a16e666d
M.AhmedAfzal Khan <mahmedafzalkhan@gmail.com>
2025-08-18 20:08:46 +0500

    Adding my airbnb clone project on github

---
+++ app.js
@@ -0,0 +1,120 @@
+// Core Module
+const path = require('path');
+
+// External Module
+const express = require('express');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const { default: mongoose } = require('mongoose');
+const multer = require('multer');
+const DB_PATH =
+  "mongodb+srv://eliteefusion:eliteefusion@cluster0.tsjhhgw.mongodb.net/airbnb?retryWrites=true&w=majority&appName=Cluster0";
+
+//Local Module
+const storeRouter = require("./routes/storeRouter")
+const hostRouter = require("./routes/hostRouter")
+const authRouter = require("./routes/authRouter")
+const rootDir = require("./utils/pathUtil");
+const errorsController = require("./controllers/errors");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: 'sessions'
+});
+
+const randomString = (length) => {
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    if (file.mimetype === "application/pdf") {
+      cb(null, "rules/"); // Directory to store PDF files
+    } else {
+      cb(null, "uploads/"); // Directory to store uploaded files
+    }
+  },
+  filename: (req, file, cb) => {
+    cb(null, randomString(10) + "-" + file.originalname); // Unique filename
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // Accept only image files and PDF files
+  if (
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "application/pdf"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const multerOptions = {
+  storage,
+  fileFilter 
+};
+
+app.use(express.urlencoded());
+// app.use(multer(multerOptions).single("photo", "homeRules")); // 'photo' is the name of the file input in the form
+app.use(
+  multer(multerOptions).fields([
+    { name: "photo", maxCount: 1 },
+    { name: "homeRules", maxCount: 1 }
+  ])
+);
+app.use(express.static(path.join(rootDir, "public")));
+app.use("/uploads", express.static(path.join(rootDir, "uploads")));
+app.use("/host/uploads", express.static(path.join(rootDir, "uploads")));
+app.use("/homes/uploads", express.static(path.join(rootDir, "uploads")));
+app.use("/rules", express.static(path.join(rootDir, "rules")));
+
+
+app.use(session({
+  secret: "KnowledgeGate AI with Complete Coding",
+  resave: false,
+  saveUninitialized: true,
+  store
+}));
+
+app.use((req, res, next) => {
+  req.isLoggedIn = req.session.isLoggedIn
+  next();
+})
+
+app.use(authRouter)
+app.use(storeRouter);
+app.use("/host", (req, res, next) => {
+  if (req.isLoggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+app.use("/host", hostRouter);
+
+app.use(errorsController.pageNotFound);
+
+const PORT = 3003;
+
+mongoose.connect(DB_PATH).then(() => {
+  console.log('Connected to Mongo');
+  app.listen(PORT, () => {
+    console.log(`Server running on address http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.log('Error while connecting to Mongo: ', err);
+});
